8547a91519d040b6657184e361686e4723786b03
rasulik <you@example.com>
2022-07-16 00:58:01 +0300

    first commit

---
+++ searchgun/PageMenu.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace searchgun
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMenu.xaml
+    /// </summary>
+    public partial class PageMenu : Page
+    {
+        public PageMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void threexthree_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void sixxsix_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ninexnine_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnFAQ_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ыв");
+        }
+
+        private void exitthisprogram_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
